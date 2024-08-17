550c2aa297e5a1b7d9e07f9e2870a0b24c9c7806
mallowwww <mallowhorton@proton.me>
2024-08-17 01:40:25 -0500

    basic file opening

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::{Menu, CustomMenuItem};
 
+use tauri::{Menu, CustomMenuItem, Submenu};
+use rfd::FileDialog;
+
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String
+}
 fn main() {
   let menu = Menu::new()
-    .add_item(CustomMenuItem::new("file", "File"));
+    .add_submenu(Submenu::new("File", Menu::new().add_item(CustomMenuItem::new("open", "Open"))));
   tauri::Builder::default()
     .menu(menu)
+    .on_menu_event(|event| {
+      event.window().emit(("frontend_".to_owned() + event.menu_item_id().into()).as_str(), Payload {message: "none".to_owned()});
+    })
+    .invoke_handler(tauri::generate_handler![getfilepath])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+#[tauri::command]
+fn getfilepath() -> String {
+  //println!("yep");
+  let mut answer = FileDialog::new()
+    .add_filter("Demo file", &["dem"])
+    .set_directory("/")
+    .pick_file();
+  return answer.expect("nope").as_path().to_string_lossy().into();
+  
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
     export const prerender = true
     export const ssr = false
+    import { fileLoaded, filePath } from "$lib/filestore"
     import "../app.css"
+    import { invoke } from '@tauri-apps/api/tauri'
+    import { onMount } from "svelte";
+    import { listen } from '@tauri-apps/api/event'
+    async function getfilepath() {
+        invoke<string>('getfilepath').then((message) => {
+            if (message != "nope") {
+                $fileLoaded = true
+                $filePath = message
+            } else {
+                $fileLoaded = false
+                $filePath = ''
+            }
+        })
+    }
+    onMount(async () => {
+        listen('frontend_open', (event) => {
+            getfilepath()
+        })
+        $fileLoaded = false
+    })
+    
 </script>
+
 <slot />
